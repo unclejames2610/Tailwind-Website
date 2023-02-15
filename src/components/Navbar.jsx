91effdc791a9e0d7f2873e4eee6943363949a98a
unclejames2610 <udealaebuka@gmail.com>
2023-02-15 17:54:40 +0100

    Navbar start

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
+
+function Navbar() {
+  return (
+    <div className="w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg">
+      <div className="px-2 flex justify-between items-center w-full h-full">
+        <div className="flex items-center">
+          <h1 className="text-3xl font-bold mr-4 sm:text-4xl">BRAND.</h1>
+        </div>
+      </div>
+      <MenuIcon className="w-5" />
+    </div>
+  );
+}
+
+export default Navbar;
