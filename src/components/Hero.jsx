4b8f56b94e025e99d6f28c6470e8af214f59116c
unclejames2610 <udealaebuka@gmail.com>
2023-02-20 17:13:11 +0100

    Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,11 +12,13 @@ const Hero = () => {
   return (
     <div className="w-full h-screen bg-zinc-200 flex flex-col justify-between">
       <div className="grid md:grid-cols-2 max-w-[1240px] m-auto">
-        <div>
-          <p>Unique Sequencing & Production</p>
-          <h1>Cloud Management</h1>
-          <p>This is our Tech brand.</p>
-          <button>Get Started</button>
+        <div className="flex flex-col justify-center md:items-start w-full px-2 py-8">
+          <p className="text-2xl">Unique Sequencing & Production</p>
+          <h1 className="py-3 text-5xl md:text-7xl font-bold">
+            Cloud Management
+          </h1>
+          <p className="text-2xl">This is our Tech brand.</p>
+          <button className="py-3 px-6 sm:w-[60%]">Get Started</button>
         </div>
       </div>
     </div>
