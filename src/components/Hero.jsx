a4ec51a24254e0d42141f2c9524e789d9c91061d
unclejames2610 <udealaebuka@gmail.com>
2023-02-27 16:10:07 +0100

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,7 +18,32 @@ const Hero = () => {
             Cloud Management
           </h1>
           <p className="text-2xl">This is our Tech brand.</p>
-          <button className="py-3 px-6 sm:w-[60%]">Get Started</button>
+          <button className="py-3 px-6 sm:w-[60%] my-4">Get Started</button>
+        </div>
+        <div>
+          <img className="w-full" src={bgImg} alt="/" />
+        </div>
+        <div
+          className="absolute flex flex-col py-8 md:min-w-[760px]
+        bottom-[5%] mx-1 md:left-1/2 transform md:-translate-x-1/2
+        bg-zinc-200 border border-slate-300 rounded-xl 
+        text-center shadow-xl"
+        >
+          <p>Data Services</p>
+          <div className="flex justify-between px-1">
+            <p className="flex px-4 py-2 text-slate-500">
+              <CloudUploadIcon className="h-6 text-indigo-600" /> App Security
+            </p>
+            <p className="flex px-4 py-2 text-slate-500">
+              <DatabaseIcon className="h-6 text-indigo-600" /> Dashboard Design
+            </p>
+            <p className="flex px-4 py-2 text-slate-500">
+              <ServerIcon className="h-6 text-indigo-600" /> Cloud Data
+            </p>
+            <p className="flex px-4 py-2 text-slate-500">
+              <PaperAirplaneIcon className="h-6 text-indigo-600" /> API
+            </p>
+          </div>
         </div>
       </div>
     </div>
