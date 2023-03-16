baef3986d124e179edebbbda11ba945de77ff565
unclejames2610 <udealaebuka@gmail.com>
2023-03-16 15:50:02 +0100

    change

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -6,7 +6,7 @@ const Pricing = () => {
     <div name="pricing" className="w-full text-white my-24">
       <div className="w-full h-[800px] bg-slate-900 absolute mix-blend-overlay"></div>
 
-      <div className="max-w-[1240px] mx-auto py-12 relative">
+      <div className="max-w-[1240px] mx-auto py-12">
         <div className="text-center py-8 text-slate-300">
           <h2 className="text-3xl uppercase">Pricing</h2>
           <h3 className="text-5xl font-bold text-white py-8">
